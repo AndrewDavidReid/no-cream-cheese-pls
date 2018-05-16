f139676ed18e9f244494215f4a1e22d92157c3c9
Andrew Reid <andrew@thinkingbig.net>
2018-05-15 22:09:37 -0300

    Closes #4 - Add home page
    * Pulling in Angular material. Going to try this out for styling things.
    * Adding a home page. This is where all shopping lists will be aggregated and we will link out to the other pages.
    * Adding a loading components and loading service
    * Adding a navbar service
    * Fuck TS lint

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavbarService } from './services/navbar.service';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'nccp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nccp';
+export class AppComponent implements OnInit
+{
+  constructor(public navbarService: NavbarService,
+              public loadingService: LoadingService)
+  { }
+
+  public NavbarTitle: string;
+  public ShowLoadingSpinner: boolean;
+
+  ngOnInit()
+  {
+    this.navbarService.Title$.subscribe(title => this.NavbarTitle = title);
+    this.loadingService.Loading$.subscribe(showLoading => this.ShowLoadingSpinner = showLoading);
+  }
 }
