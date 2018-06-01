6e30b2a70453e6cc881900bb97e1ccd771e26047
Andrew Reid <andrew@thinkingbig.net>
2018-06-01 12:23:18 -0300

    * Pushing up a bug fix around httpclient

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
 } from '@angular/material';
 import { HomeComponent } from './components/home/home.component';
 import { LoadingOverlayComponent } from './components/loading-overlay/loading-overlay.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { LoadingOverlayComponent } from './components/loading-overlay/loading-ov
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatMenuModule,
